61da129e2523a9537e61bf8fcb71697d65a983ef
Mahdi Hellal <mahdihellal@gmail.com>
2020-07-27 15:47:20 +0100

    add pdf

--- src/containers/ProfilContainer/containers/CardContainer/styles.ts
+++ src/containers/ProfilContainer/containers/CardContainer/styles.ts
@@ -35,23 +35,6 @@ export default makeStyles((theme) => ({
     fontFamily: 'Ocean',
     textTransform: 'uppercase',
   },
-  headerIcons: {
-    gridArea: 'icons',
-    display: 'flex',
-  },
-  headerIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#6B6B6A',
-    marginLeft: 22,
-    cursor: 'pointer',
-  },
-  headerIconImage: {
-    width: 30,
-    marginRight: 11,
-  },
   card: {
     marginTop: 24,
     display: 'flex',
@@ -59,29 +42,12 @@ export default makeStyles((theme) => ({
     padding: 40,
     color: 'inherit',
   },
-  cardHeader: {
+
+  footerIcons: {
+    width: '100%',
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderBottom: '1px solid #6B6B6A',
-    paddingBottom: 20,
-  },
-  userInfo: {
-    display: 'flex',
-    flexDirection: 'column',
-    whiteSpace: 'pre-wrap',
-  },
-  userName: {
-    fontWeight: 'bold',
-    textTransform: 'capitalize',
-    fontSize: 18,
-  },
-  appInfo: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  appLogo: {
-    marginRight: 30,
+    justifyContent: 'flex-end',
+    marginTop: 40,
   },
   part: {
     padding: '30px 0',
@@ -91,138 +57,5 @@ export default makeStyles((theme) => ({
     '&:last-child': {
       borderBottom: 'none',
     },
-  },
-  competencesPart: {
-    width: '50%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  subTitle: {
-    color: theme.palette.background.default,
-    fontSize: 12,
-  },
-  competences: { marginTop: 25 },
-  competenceTitle: {
-    marginBottom: 10,
-    fontSize: 14,
-    lineHeight: '20px',
-    fontWeight: 'bold',
-  },
-  competenceNiveau: {
-    fontSize: 10,
-    width: '100%',
-    lineHeight: '13px',
-    marginTop: 10,
-  },
-  emptyCompetences: {
-    fontSize: 18,
-    lineHeight: '29px',
-    marginTop: 17,
-  },
-  emptyCompetencesBold: { fontWeight: 'bold' },
-  skillsContainer: { marginTop: 16 },
-  skillHeader: {
-    display: 'flex',
-    width: '100%',
-    alignItems: 'center',
-  },
-  themeTitle: {
-    flex: '1 1 0%',
-    fontWeight: 'bold',
-    fontSize: 18,
-    lineHeight: '26px',
-    marginLeft: 14,
-    display: 'flex',
-    alignItems: 'center',
-  },
-  themeTitlePro:{
-    marginLeft: 0,
-
-  },
-  titleText: {
-    maxWidth: 'calc(100% - 37px)',
-  },
-  commentIcon: {
-    marginLeft: 7,
-    width: 30,
-  },
-  themeIcon: {
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
-  activityContainer: {
-    marginTop: 9,
-    marginLeft: 5,
-  },
-  activity: {
-    fontSize: 10,
-    paddingLeft: 6,
-    position: 'relative',
-    '&:before': {
-      backgroundColor: '#424242',
-      content: '""',
-      height: 3,
-      width: 3,
-      borderRadius: 1.5,
-      position: 'absolute',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      left: 0,
-    },
-  },
-  emptyMessage: {
-    marginTop: 25,
-    fontSize: 18,
-    lineHeight: '29px',
-    marginBottom: 18,
-  },
-  emptyButton: {
-    backgroundColor: '#00CFFF',
-    borderRadius: 10,
-    color: '#223A7A',
-    fontSize: 18,
-    boxShadow: 'none',
-    textTransform: 'none',
-    '&:hover': {
-      backgroundColor: '#00B2DB',
-      color: '#011A5E',
-    },
-  },
-  footerIcons: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginTop: 40,
-  },
-  tooltip: {
-    left: '-10vw !important',
-  },
-  skill: {
-    borderRadius: 10,
-    cursor: 'pointer',
-    '&:hover': {
-      backgroundColor: 'rgba(122, 230, 255, 0.2)',
-    },
-  },
-  tooltipContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  tooltipTitle: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    lineHeight: '15px',
-    WebkitTextStroke: '0.2px',
-  },
-  tooltipUser: {
-    color: '#7AE6FF',
-    textTransform: 'capitalize',
-  },
-  tooltipLocation: {
-    fontWeight: 'normal',
-  },
-  tooltipCommentText: {
-    fontSize: 12,
-    marginTop: 13,
   },
 }));

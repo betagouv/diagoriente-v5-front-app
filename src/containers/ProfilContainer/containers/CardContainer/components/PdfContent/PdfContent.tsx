e457f2d075d8b7b4a485d0eaec3bfc4e64165894
mahdi hellal <mahdihellal@gmail.com>
2020-07-29 15:08:11 +0100

    add contact pdf

--- src/containers/ProfilContainer/containers/CardContainer/components/PdfContent/PdfContent.tsx
+++ src/containers/ProfilContainer/containers/CardContainer/components/PdfContent/PdfContent.tsx
@@ -1,4 +1,5 @@
-import React, { forwardRef, Ref, useContext } from 'react';
+import { createPortal } from 'react-dom';
+import React, { useContext, forwardRef, Ref } from 'react';
 import ParcourContext from 'contexts/ParcourContext';
 import classNames from 'utils/classNames';
 
@@ -23,43 +24,47 @@ const PdfContent = forwardRef((props, ref: Ref<HTMLDivElement>) => {
       ...parcours?.skills.map((skill) => skill.comment.map((comment) => ({ title: skill.theme.title, comment }))),
     ) || []
   ).filter(({ comment }) => comment.status === 'accepted');
-  return (
-    <div className={classes.container} ref={ref}>
-      <CardHeader>
-        <div className={classNames(classes.headerTitle, classes.pdfTitle)}>
-          <img className={classes.headerImage} src={carte} alt="" />
-          <span className={classes.title}>CARTE DE COMPÉTENCES</span>
-        </div>
-      </CardHeader>
 
-      <CardCompetence />
-      <CardSkills
-        title="Expériences personnelles"
-        type="personal"
-        emptyMessage="Tu n’as pas encore renseigné d'expérience personnelle"
-        emptyButton="J’ajoute une expérience perso"
-        path=""
-      />
-      <CardSkills
-        title="Expériences professionnelles"
-        type="professional"
-        emptyMessage="Tu n’as pas encore renseigné d'expérience professionnelle"
-        emptyButton="J’ajoute une expérience pro"
-        path=""
-      />
-      <CardPart title="RECOMMANDATIONS">
-        <Grid className={classes.commentContainer} container spacing={3}>
-          {comments.map(({ comment, title }) => (
-            <Grid key={comment.id} item lg={4}>
-              <div className={classes.themeTitle}>{title}</div>
-              <Comment className={classes.comment} {...comment}>
-                <img src={medaille} alt="" className={classes.commentIcon} />
-              </Comment>
-            </Grid>
-          ))}
-        </Grid>
-      </CardPart>
-    </div>
+  return createPortal(
+    <div className={classes.container}>
+      <div ref={ref} className={classes.pdf}>
+        <CardHeader>
+          <div className={classNames(classes.headerTitle, classes.pdfTitle)}>
+            <img className={classes.headerImage} src={carte} alt="" />
+            <span className={classes.title}>CARTE DE COMPÉTENCES</span>
+          </div>
+        </CardHeader>
+
+        <CardCompetence />
+        <CardSkills
+          title="Expériences personnelles"
+          type="personal"
+          emptyMessage="Tu n’as pas encore renseigné d'expérience personnelle"
+          emptyButton="J’ajoute une expérience perso"
+          path=""
+        />
+        <CardSkills
+          title="Expériences professionnelles"
+          type="professional"
+          emptyMessage="Tu n’as pas encore renseigné d'expérience professionnelle"
+          emptyButton="J’ajoute une expérience pro"
+          path=""
+        />
+        <CardPart title="RECOMMANDATIONS">
+          <Grid className={classes.commentContainer} container spacing={3}>
+            {comments.map(({ comment, title }) => (
+              <Grid key={comment.id} item lg={4}>
+                <div className={classes.themeTitle}>{title}</div>
+                <Comment className={classes.comment} {...comment}>
+                  <img src={medaille} alt="" className={classes.commentIcon} />
+                </Comment>
+              </Grid>
+            ))}
+          </Grid>
+        </CardPart>
+      </div>
+    </div>,
+    document.getElementById('root') as HTMLDivElement,
   );
 });
 

3b0118b4f1034b5e4867e712778f39b7afbf541b
Mahdi Hellal <mahdihellal@gmail.com>
2020-08-05 11:40:47 +0100

    add first use dashboard

--- src/components/layout/PrivateDrawer/PrivateDrawer.tsx
+++ src/components/layout/PrivateDrawer/PrivateDrawer.tsx
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 
 import Drawer from '@material-ui/core/Drawer';
+import Button from 'components/button/Button';
 import List from '@material-ui/core/List';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import localforage from 'localforage';
 import DrawerContext from 'contexts/DrawerContext';
 import parcoursContext from 'contexts/ParcourContext';
+import Img from 'assets/images/fleche_fu.png';
 import userContext from 'contexts/UserContext';
-import { setAuthorizationBearer } from 'requests/client';
-import { client } from 'requests/client';
+import { client, setAuthorizationBearer } from 'requests/client';
+import classNames from 'utils/classNames';
 
 import useStyles from './styles';
 
@@ -20,14 +22,16 @@ export const links = [
 ];
 
 const PrivateDrawer = () => {
+  const location = useLocation();
+  const path = location.pathname.split(/[//]/)[1];
   const classes = useStyles();
   /* const [variant, setVariant] = useState<'persistent' | 'temporary'>(
     window.innerWidth < 768 ? 'temporary' : 'persistent',
   ); */
   const { open, setOpen } = useContext(DrawerContext);
-  const { setParcours } = useContext(parcoursContext);
+  const { setParcours, parcours } = useContext(parcoursContext);
   const { setUser } = useContext(userContext);
-
+  const ParcourRef = useRef(parcours?.completed);
   const logout = () => {
     localforage.removeItem('auth');
     setAuthorizationBearer('');
@@ -44,32 +48,64 @@ const PrivateDrawer = () => {
   const onClose = () => {
     setOpen(false);
   };
+  useEffect(() => {
+    if (!ParcourRef.current) {
+      if (parcours?.completed) {
+        setOpen(true);
+      }
+    }
+  }, [parcours?.completed, setOpen, parcours]);
 
   return (
-    <Drawer
-      variant="temporary"
-      anchor="top"
-      open={open}
-      classes={{
-        paper: classes.drawerPaper,
-        root: classes.root,
-      }}
-      ModalProps={{
-        keepMounted: true,
-      }}
-      onClose={onClose}
-    >
-      <div className={classes.toolbar} />
-      <List className={classes.root}>
-        {links.map((e) => (
-          <li key={e.text} className={classes.linkContainer} onClick={e.text === 'DÉCONNEXION' ? logout : () => {}}>
-            <Link className={classes.link} to={e.path}>
-              {e.text}
-            </Link>
-          </li>
-        ))}
-      </List>
-    </Drawer>
+    <>
+      <Drawer
+        variant="temporary"
+        anchor="top"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+          root: classes.root,
+        }}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        onClose={onClose}
+      >
+        <div className={classes.toolbar} />
+        <List className={classes.root}>
+          {links.map((e) => (
+            <li key={e.text} className={classes.linkContainer} onClick={e.text === 'DÉCONNEXION' ? logout : () => {}}>
+              <Link to={e.path}>
+                <div
+                  className={classNames(
+                    path === 'jobs' ? classes.linkJob : classes.link,
+                    !ParcourRef.current && parcours?.completed && e.text === 'MON DASHBOARD' && classes.firstUseLink,
+                  )}
+                >
+                  {e.text}
+                </div>
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Drawer>
+      {!ParcourRef.current && parcours?.completed && open && (
+        <div style={{ position: 'absolute', top: 65, left: 200, zIndex: 9999, display: 'flex' }}>
+          <div>
+            <img alt="" src={Img} />
+          </div>
+          <div>
+            <div className={classes.textMsg}>
+              Pour compléter ton profil et retrouver à tout moment toutes tes informations, rend toi dans ton tableau de
+              bord, accessible via le menu.
+            </div>
+            <Button className={classes.btn}>
+              <div className={classes.btnLabel}>Compris !</div>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default PrivateDrawer;

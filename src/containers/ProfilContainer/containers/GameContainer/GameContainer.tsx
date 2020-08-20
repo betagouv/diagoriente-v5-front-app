947168cda13dd74e9e0cb0333ef758eed690b6e5
mahdi hellal <mahdihellal@gmail.com>
2020-08-20 13:29:43 +0100

    add img to interest

---
+++ src/containers/ProfilContainer/containers/GameContainer/GameContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import useStyle from './style';
+
+const GameContainer = () => {
+  const classes = useStyle();
+  return <div>test</div>;
+};
+
+export default GameContainer;

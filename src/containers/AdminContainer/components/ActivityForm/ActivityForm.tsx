5e02ba502c749f2e2319ad1f6fbba4ee435ba58f
Mahdi Hellal <mahdihellal@gmail.com>
2020-08-25 12:20:38 +0100

    remove options

--- src/containers/AdminContainer/components/ActivityForm/ActivityForm.tsx
+++ src/containers/AdminContainer/components/ActivityForm/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import { THEME_TYPES_OPTIONS } from 'utils/generic';
 
@@ -6,20 +6,12 @@ import { useForm } from 'hooks/useInputs';
 import { useInterests } from 'requests/interests';
 import { Activity } from 'requests/types';
 
-import Paper from '@material-ui/core/Paper/Paper';
-import Fab from '@material-ui/core/Fab/Fab';
 import AdminTextField from 'components/inputs/AdminTextField/AdminTextField';
 import AdminCheckBox from 'components/inputs/AdminCheckbox/AdminCheckbox';
 import Button from '@material-ui/core/Button/Button';
 import AdminSelect from 'components/inputs/AdminSelect/AdminSelect';
 import AdminAutocomplete from 'components/inputs/AdminAutocomplete/AdminAutocomplete';
 
-import Done from '@material-ui/icons/Done';
-import Clear from '@material-ui/icons/Clear';
-import Add from '@material-ui/icons/Add';
-import Remove from '@material-ui/icons/Remove';
-
-import { Tooltip } from '@material-ui/core';
 import useStyles from './styles';
 
 interface ActivityFormValues {
@@ -28,7 +20,6 @@ interface ActivityFormValues {
   type: string;
   verified: boolean;
   interests: string[];
-  options?: { value: string; verified: boolean }[];
 }
 
 interface ThemeFormProps {
@@ -38,7 +29,6 @@ interface ThemeFormProps {
 
 const ActivityForm = ({ onSubmit, activity }: ThemeFormProps) => {
   const classes = useStyles();
-  const [value, setValue] = useState('');
   const [state, actions] = useForm({
     initialValues: {
       title: '',
@@ -46,7 +36,6 @@ const ActivityForm = ({ onSubmit, activity }: ThemeFormProps) => {
       type: '',
       verified: false,
       interests: [] as { label: string; value: string }[],
-      options: [] as { value: string; verified: boolean }[],
     },
   });
   const { values } = state;
@@ -56,7 +45,6 @@ const ActivityForm = ({ onSubmit, activity }: ThemeFormProps) => {
     if (activity) {
       setValues({
         ...activity,
-        options: activity.options?.map((opt) => ({ value: opt.value, verified: opt.verified })) || [],
         interests: activity.interests.map((interest) => ({
           label: interest.nom,
           value: interest.id,
@@ -69,13 +57,7 @@ const ActivityForm = ({ onSubmit, activity }: ThemeFormProps) => {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const data = { ...values };
-    if (data.type !== 'engagement') delete data.options;
     onSubmit({ ...data, interests: values.interests.map((interest) => interest.value) });
-  }
-
-  function addOption() {
-    setValues({ options: [...values.options, { value, verified: true }] });
-    setValue('');
   }
 
   return (
@@ -119,55 +101,6 @@ const ActivityForm = ({ onSubmit, activity }: ThemeFormProps) => {
           onChange={(e, v) => setValues({ interests: v })}
           className={classes.interests}
         />
-        <div className={classes.options}>
-          <AdminTextField
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter' && value.length > 2) {
-                e.preventDefault();
-                addOption();
-              }
-            }}
-            InputProps={{
-              className: classes.optionsInput,
-              endAdornment: (
-                <Fab onClick={addOption} size="small" className={classes.add} disabled={value.length < 3}>
-                  <Add />
-                </Fab>
-              ),
-            }}
-            label="Options"
-          />
-          {values.options.map((option, index) => (
-            // eslint-disable-next-line
-            <div key={index} className={classes.option}>
-              <Paper className={classes.optionValue}>{option.value}</Paper>
-              <Tooltip title={option.verified ? 'Vérifié' : 'Non vérifié'}>
-                <Fab
-                  onClick={() => {
-                    const nextOptions = [...values.options];
-                    nextOptions[index] = { ...values.options[index], verified: !option.verified };
-                    setValues({ options: nextOptions });
-                  }}
-                  size="small"
-                  className={option.verified ? classes.optionVerify : classes.optionClear}
-                >
-                  {option.verified ? <Done /> : <Clear />}
-                </Fab>
-              </Tooltip>
-              <Tooltip title="Supprimer">
-                <Fab
-                  onClick={() => setValues({ options: values.options.filter((o, i) => index !== i) })}
-                  size="small"
-                  className={classes.optionRemove}
-                >
-                  <Remove />
-                </Fab>
-              </Tooltip>
-            </div>
-          ))}
-        </div>
 
         <AdminCheckBox
           name="verified"

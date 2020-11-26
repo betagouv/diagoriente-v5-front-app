db1d291d1811435a616e9450469f881ddb835edd
mahdi hellal <mahdihellal@gmail.com>
2020-11-26 10:11:00 +0100

    remove unused vars

--- src/containers/AdminContainer/containers/ParcoursContainer/Parcours.tsx
+++ src/containers/AdminContainer/containers/ParcoursContainer/Parcours.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { Header } from 'components/ui/Table/Table';
-import Crud from 'components/ui/Crud/Crud';
 
 const Parcours = () => {
-  return <div>parcours</div>;
+  return <div>parcour</div>;
 };
 
 export default Parcours;

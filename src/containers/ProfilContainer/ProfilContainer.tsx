779a824965ad7a32122c9f2bec38f1b9518e74b2
mahdihellal <mahdihellal@gmail.com>
2020-12-01 15:46:39 +0100

    fix header in profile container

--- src/containers/ProfilContainer/ProfilContainer.tsx
+++ src/containers/ProfilContainer/ProfilContainer.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Route from 'components/ui/Route/Route';
 import { Switch } from 'react-router-dom';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import NotFoundPage from 'components/layout/NotFoundPage';
+import userContext from 'contexts/UserContext';
 
 import ExperienceProfil from './components/ExperienceComponent/ExperienceProfil';
 import ProfilComponent from './components/ProfilComponent/ProfilComponent';
@@ -11,6 +12,11 @@ import InfoProfil from './components/InfoProfil/InfoProfil';
 import Game from './containers/GameContainer/GameContainer';
 
 import CardContainer from './containers/CardContainer';
+
+import logo from 'assets/svg/diagoriente_logo_01_bg_transparent 2.svg';
+import logCampus from 'assets/images/diagorient-campus.png';
+import open from 'assets/svg/menu_close.svg';
+import whiteMenu from 'assets/images/menu.png';
 
 const theme = createMuiTheme({
   palette: {
@@ -44,18 +50,62 @@ const theme = createMuiTheme({
   },
 });
 
-const Profil = () => (
-  <ThemeProvider theme={theme}>
-    <Switch>
-      <Route protected exact path="/profile" component={ProfilComponent} />
-      <Route protected exact path="/profile/experience" component={ExperienceProfil} />
-      <Route protected exact path="/profile/interest" component={InteretProfil} />
-      <Route protected exact path="/profile/info" component={InfoProfil} />
-      <Route protected exact path="/profile/game" component={Game} />
-      <Route footer protected exact path="/profile/card" component={CardContainer} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  </ThemeProvider>
-);
+const Profil = () => {
+  const { user } = useContext(userContext);
+  return (
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route protected exact path="/profile" component={ProfilComponent} />
+        <Route
+          privateHeaderProps={{
+            closeLogoIcon: user?.isCampus ? logCampus : logo,
+            openIcon: user?.isCampus ? whiteMenu : open,
+            showUser: false,
+          }}
+          protected
+          exact
+          path="/profile/experience"
+          component={ExperienceProfil}
+        />
+        <Route protected exact path="/profile/interest" component={InteretProfil} />
+        <Route
+          privateHeaderProps={{
+            closeLogoIcon: user?.isCampus ? logCampus : logo,
+            openIcon: user?.isCampus ? whiteMenu : open,
+            showUser: false,
+          }}
+          protected
+          exact
+          path="/profile/info"
+          component={InfoProfil}
+        />
+        <Route
+          privateHeaderProps={{
+            closeLogoIcon: user?.isCampus ? logCampus : logo,
+            openIcon: user?.isCampus ? whiteMenu : open,
+            showUser: false,
+          }}
+          protected
+          exact
+          path="/profile/game"
+          component={Game}
+        />
+        <Route
+          privateHeaderProps={{
+            closeLogoIcon: user?.isCampus ? logCampus : logo,
+            openIcon: user?.isCampus ? whiteMenu : open,
+            showUser: false,
+          }}
+          footer
+          protected
+          exact
+          path="/profile/card"
+          component={CardContainer}
+        />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </ThemeProvider>
+  );
+};
 
 export default Profil;

f29f91ae294b43e976f7e0ed868134de5381573d
mahdihellal <mahdihellal@gmail.com>
2021-01-25 10:29:37 +0100

    remove chart contrat

--- src/utils/createLazyComponent.tsx
+++ src/utils/createLazyComponent.tsx
@@ -1,12 +1,10 @@
-import React, {
- Suspense, lazy, ComponentType, ComponentProps,
-} from 'react';
+import React, { Suspense, lazy, ComponentType, ComponentProps } from 'react';
 import Loader from 'components/layout/Loader/Loader';
 
-export default function<T extends ComponentType<any>> (path: () => Promise<{ default: T }>) {
+export default function<T extends ComponentType<any>>(path: () => Promise<{ default: T }>) {
   const Component = lazy(path);
 
-  return function (props: ComponentProps<T>) {
+  return function(props: ComponentProps<T>) {
     return (
       <Suspense fallback={<Loader />}>
         <Component {...props} />

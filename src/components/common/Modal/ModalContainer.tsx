bdd7a8302059fd626beb75e0fae6939b2a08c90d
Mahdi Hellal <mahdihellal@gmail.com>
2021-03-01 19:21:14 +0100

    add script

--- src/components/common/Modal/ModalContainer.tsx
+++ src/components/common/Modal/ModalContainer.tsx
@@ -30,7 +30,7 @@ const ModalContainer = ({
   const classes = useStyles({ backdropColor, size, height });
   return (
     <Modal
-      style={{ zIndex: 1300 }}
+      style={{ zIndex: 1350 }}
       open={open}
       onClose={handleClose}
       disablePortal
